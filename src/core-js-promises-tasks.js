b394e38774248478ce959457a4ee35b99fb484f2
Maksim Sukhov <silveraven@yandex.ru>
2025-03-28 23:32:37 +0200

    feat: add solution getPromise

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,14 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  return new Promise((fulfield, rejected) => {
+    if (number >= 0) {
+      fulfield('promise that will be fulfilled');
+    } else {
+      rejected(new Error('promise that will be rejected'));
+    }
+  });
 }
 
 /**
