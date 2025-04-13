3e1916a53e7b1db7ffd3459c9f138dffb947c202
Maksim Sukhov <silveraven@yandex.ru>
2025-04-13 22:16:51 +0200

    feat: add solution getFirstResolvedPromiseResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -62,8 +62,11 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(romises) {
+  const promise = Promise.any(romises);
+  return promise.then((item) => {
+    return item;
+  });
 }
 
 /**
